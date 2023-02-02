01052181480ef59f04ecded3eb2a82047c7264db
olashina201 <olashina201@gmail.com>
2023-02-02 10:23:34 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -82,60 +82,6 @@ async function main() {
     const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
     const cntr = new ethers.Contract(process.env.CONTRACT_ADDRESS as string, CntrAbi, signer);
 
-    // ============ wMATIC/MANA ============
-    {
-        console.log('wMATIC/MANA');
-        const dat: any = await runMath(5000, [
-            // https://info.uniswap.org/#/polygon/pools/0x56845fd95C766eCB0ab450fE2D105a19440a6E35
-            await uniswapV3Price(poolContract('0x56845fd95C766eCB0ab450fE2D105a19440a6E35', IUniswapV3PoolABI), 18, 18, 3000),
-            // https://info.quickswap.exchange/#/pair/0x6b0Ce31eAD9b14c2281D80A5DDE903AB0855313A
-            await uniswapV2Price(poolContract('0x6b0Ce31eAD9b14c2281D80A5DDE903AB0855313A', IUniswapV2PairABI), 3000)
-        ]);
-        if (dat != null) {
-            dat.buy["tokenIn"] = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270';
-            dat.sell["tokenIn"] = '0xA1c57f48F0Deb89f569dFbE6E2B7f46D33606fD4';
-
-            let baseNonce = provider.getTransactionCount(signer.address);
-            let nonceOffset = 0;
-
-            await cntr.functions.execute(dat, ethers.utils.parseUnits('5000', 18), { gasLimit: process.env.GAS_LIMIT, nonce: await getNonce(signer.address) }).catch(console.error);
-        }
-    }
-    
-    // ============ wETH/MANA ============
-    {
-        console.log('wETH/MANA');
-        const dat: any = await runMath(0.5, [
-            // https://info.uniswap.org/#/polygon/pools/0x28bdd3749bdea624a726ca153de1cb673f459b9d
-            await uniswapV3Price(poolContract('0x28bdd3749bdea624a726ca153de1cb673f459b9d', IUniswapV3PoolABI), 18, 18, 3000),
-            // https://info.quickswap.exchange/#/pair/0x814b6c10bf752bbbea7bd68e5e65dc28b4f45982
-            await uniswapV2Price(poolContract('0x814b6c10bf752bbbea7bd68e5e65dc28b4f45982', IUniswapV2PairABI), 3000)
-        ]);
-        if (dat != null) {
-            dat.buy["tokenIn"] = '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619';
-            dat.sell["tokenIn"] = '0xa1c57f48f0deb89f569dfbe6e2b7f46d33606fd4';
-
-            await cntr.functions.execute(dat, ethers.utils.parseUnits('0.5', 18), { gasLimit: process.env.GAS_LIMIT, nonce: await getNonce(signer.address) }).catch(console.error);
-        }
-    }
-
-    // ============ wMATIC/AVAX ============
-    {
-        console.log('wMATIC/AVAX');
-        const dat: any = await runMath(800, [
-            // https://info.uniswap.org/#/polygon/pools/0xfa3f210cbad19c8b860a256d67a400d616a87c2a
-            await uniswapV3Price(poolContract('0xfa3f210cbad19c8b860a256d67a400d616a87c2a', IUniswapV3PoolABI), 18, 18, 3000),
-            // https://info.quickswap.exchange/#/pair/0xeb477ae74774b697b5d515ef8ca09e24fee413b5
-            await uniswapV2Price(poolContract('0xeb477ae74774b697b5d515ef8ca09e24fee413b5', IUniswapV2PairABI), 3000)
-        ]);
-        if (dat != null) {
-            dat.buy["tokenIn"] = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
-            dat.sell["tokenIn"] = '0x2c89bbc92bd86f8075d1decc58c7f4e0107f286b';
-
-            await cntr.functions.execute(dat, ethers.utils.parseUnits('800', 18), { gasLimit: process.env.GAS_LIMIT, nonce: await getNonce(signer.address) }).catch(console.error);
-        }
-    }
-
     // ============ USDC/wETH ============
     {
         console.log('USDC/wETH');
@@ -194,15 +140,6 @@ async function main() {
     // https://info.uniswap.org/#/polygon/pools/0xa374094527e1673a86de625aa59517c5de346d32
     // https://info.quickswap.exchange/#/pair/0x6e7a5fafcec6bb1e78bae2a1f0b612012bf14827
 
-    // ============ wMATIC/wETH ============
-    // https://info.uniswap.org/#/polygon/pools/0x167384319b41f7094e62f7506409eb38079abff8
-    // https://info.quickswap.exchange/#/pair/0xadbf1854e5883eb8aa7baf50705338739e558e5b
-
-    // ============ wMATIC/USDT ============
-    // https://info.uniswap.org/#/polygon/pools/0x781067ef296e5c4a4203f81c593274824b7c185d
-    // https://info.quickswap.exchange/#/pair/0x604229c960e5cacf2aaeac8be68ac07ba9df81c3
-
-    // ========================
     runCounter++;
     console.log(`(${runCounter}/${oppCounter}) Finished. Awaiting next call.`);
 }
